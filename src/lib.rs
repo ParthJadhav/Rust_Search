9922c6e19d4cef7d57b891ac9e2f0787834280ee
Juliette Cordor <grassojames5@gmail.com>
2022-10-29 23:55:34 +1100

    remove redundant use of regexbuilder

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use ignore::{WalkBuilder, WalkState};
-use regex::{Regex, RegexBuilder};
+use regex::Regex;
 
 pub fn get_paths(
     search_location: impl AsRef<Path>,
@@ -52,5 +52,5 @@ fn build_regex_search_input(search_input: Option<&str>, file_type: Option<&str>)
     let search_input = search_input.unwrap_or(r"\w+\");
 
     let formatted_search_input = format!(r#"{}{}$"#, search_input, file_type);
-    RegexBuilder::new(&formatted_search_input).build().unwrap()
+    Regex::new(&formatted_search_input).unwrap()
 }
