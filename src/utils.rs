6e4bc0ec665b94b3c8a8d1429eca46654168b248
TheAwiteb <awiteb@hotmail.com>
2022-10-30 20:44:07 +0300

    Rename argument `file_type` -> `file_ext`

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,9 @@ use regex::Regex;
 
 pub(crate) fn build_regex_search_input(
     search_input: Option<&str>,
-    file_type: Option<&str>,
+    file_ext: Option<&str>,
 ) -> Regex {
-    let file_type = file_type.unwrap_or(".*");
+    let file_type = file_ext.unwrap_or(".*");
     let search_input = search_input.unwrap_or(r"\w+\");
 
     let formatted_search_input = format!(r#"{}{}$"#, search_input, file_type);
