d67cc63e5905bc97e5ee772bf1d5de6439f47d8b
TheAwiteb <awiteb@hotmail.com>
2022-10-30 21:01:29 +0300

    Export `SearchBuilder`

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 // Use the readme as the crate documentation
 #![doc = include_str!("../README.md")]
 
+mod builder;
 mod search;
 mod utils;
 
+pub use builder::SearchBuilder;
 pub use search::Search;
